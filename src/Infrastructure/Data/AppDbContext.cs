0da6b13f6a897838420c566c2cab7e91355e7f25
Siarhei Naumau <s.naumau@gmail.com>
2020-07-11 23:17:16 +0300

    feat: implement application context

---
+++ src/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(
+                        @"Server=WINBPO\DEV;Database=OnlineStore;Trusted_Connection=True;MultipleActiveResultSets=true",
+                        options => options.EnableRetryOnFailure());
+            }
+        }
+    }
+}
