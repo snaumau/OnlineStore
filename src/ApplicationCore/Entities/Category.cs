7b22bcc8b8750a3c7119a87f4b667bb7ef44f4e4
Siarhei Naumau <s.naumau@gmail.com>
2020-07-11 18:23:38 +0300

    feat: add category model

---
+++ src/ApplicationCore/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ApplicationCore.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string ThumbnailUrl { get; set; }
+    }
+}
