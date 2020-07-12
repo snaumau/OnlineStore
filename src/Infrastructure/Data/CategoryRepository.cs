6043b814f97c0c520c88dfa62a091a4ba530d2c6
Siarhei Naumau <s.naumau@gmail.com>
2020-07-12 17:35:19 +0300

    feat: implement category interface

---
+++ src/Infrastructure/Data/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using System.Collections.Generic;
+
+namespace Infrastructure.Data
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Category> GetAll => _context.Categories;
+    }
+}
