0ef7b40849c20474d0740fa385d688a0db46cd08
Siarhei Naumau <s.naumau@gmail.com>
2020-07-17 19:37:50 +0300

    feat: add home controller

---
+++ src/Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public HomeController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                AllCategories = _categoryRepository.GetAll
+            };
+            return View(homeViewModel);
+        }
+    }
+}
