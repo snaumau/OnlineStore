db452792ab034e171a36b400d9874218ac2d43aa
Siarhei Naumau <s.naumau@gmail.com>
2020-07-17 19:33:55 +0300

    feat: add home view model

---
+++ src/Web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ApplicationCore.Entities;
+using System.Collections.Generic;
+
+namespace Web.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Category> AllCategories { get; set; }
+    }
+}
