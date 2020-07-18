ed09c2e5b04116968ad2e1ee31ff0845d8b248e8
Siarhei Naumau <s.naumau@gmail.com>
2020-07-18 17:27:13 +0300

    feat: add product migrations

--- src/Infrastructure/Data/AppDbContext.cs
+++ src/Infrastructure/Data/AppDbContext.cs
@@ -12,6 +12,7 @@ namespace Infrastructure.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
