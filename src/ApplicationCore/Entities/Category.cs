bf0b8a85d6dd696d8cd548079f509416fb31757e
Siarhei Naumau <s.naumau@gmail.com>
2020-07-18 14:14:54 +0300

    feat: update category model

--- src/ApplicationCore/Entities/Category.cs
+++ src/ApplicationCore/Entities/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace ApplicationCore.Entities
+﻿using System.Collections.Generic;
+
+namespace ApplicationCore.Entities
 {
     public class Category
     {
         public int CategoryId { get; set; }
         public string Name { get; set; }
         public string ThumbnailUrl { get; set; }
+        public List<Product> Products { get; set; }
     }
 }
