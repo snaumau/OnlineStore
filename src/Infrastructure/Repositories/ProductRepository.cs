58e8d1c2ca402237af2d1a3be52f3d7d4e938280
Siarhei Naumau <s.naumau@gmail.com>
2020-07-19 13:37:16 +0300

    feat: implement product interface

---
+++ src/Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ProductRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Product> Products =>
+            _appDbContext.Products.Include(c => c.Category);
+
+        public Product GetProductById(int productId)
+        {
+            return _appDbContext.Products.FirstOrDefault(p => p.ProductId == productId);
+        }
+    }
+}
