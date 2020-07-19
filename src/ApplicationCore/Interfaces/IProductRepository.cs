957de73604e48ff44806fc73d7f3864d15869afb
Siarhei Naumau <s.naumau@gmail.com>
2020-07-19 13:31:51 +0300

    feat: add product interface

---
+++ src/ApplicationCore/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationCore.Entities;
+using System.Collections.Generic;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> Products { get; }
+
+        Product GetProductById(int productId);
+    }
+}
