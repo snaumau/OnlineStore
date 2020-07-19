ec6856d45eb225e7d7bc9d24c9a97f5e94cad894
Siarhei Naumau <s.naumau@gmail.com>
2020-07-19 14:01:48 +0300

    feat: add product controller

---
+++ src/Web/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List(string category)
+        {
+            IEnumerable<Product> products;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                products = _productRepository.Products
+                    .OrderBy(p => p.ProductId);
+                currentCategory = "All products";
+            }
+            else
+            {
+                products = _productRepository.Products
+                    .Where(p => p.Category.Name == category)
+                    .OrderBy(p => p.ProductId);
+
+                currentCategory = _categoryRepository.GetAll
+                    .FirstOrDefault(c => c.Name == category)?.Name;
+            }
+            return View(new ProductListViewModel
+            {
+                Products = products,
+                CurrentCategory = currentCategory
+            });
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = _productRepository.GetProductById(id);
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+    }
+}
