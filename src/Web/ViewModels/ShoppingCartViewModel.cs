8ba0f7203ad6517cc8b65ab44c53d86612722fbe
Siarhei Naumau <s.naumau@gmail.com>
2020-08-01 21:32:19 +0300

    feat: add shopping cart view model

---
+++ src/Web/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Infrastructure.Services;
+
+namespace Web.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCartService ShoppingCart { get; set; }
+        public decimal ShoppingCartTotal { get; set; }
+    }
+}
