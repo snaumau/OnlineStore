1de884fc90508f349d05f3ab4cbfd067e035a8df
Siarhei Naumau <s.naumau@gmail.com>
2020-08-01 16:30:01 +0300

    feat: add shopping cart model

---
+++ src/ApplicationCore/Entities/ShoppingCartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplicationCore.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int ShoppingCartItemId { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public string ShoppingCartId { get; set; }
+    }
+}
