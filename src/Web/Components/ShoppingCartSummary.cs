193e8f2c688459b2a29240eeaeaa304dae999391
Siarhei Naumau <s.naumau@gmail.com>
2020-08-02 00:12:15 +0300

    feat: add shopping cart components

---
+++ src/Web/Components/ShoppingCartSummary.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using Web.ViewModels;
+
+namespace Web.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCartService _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCartService shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal(),
+            };
+            return View(shoppingCartViewModel);
+        }
+    }
+}
