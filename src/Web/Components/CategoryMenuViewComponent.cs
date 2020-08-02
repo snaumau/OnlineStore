4afcd34b00344bfbe0f3844aa0834f46f171e4b5
Siarhei Naumau <s.naumau@gmail.com>
2020-08-02 14:34:38 +0300

    feat: add category menu view component

---
+++ src/Web/Components/CategoryMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Web.Components
+{
+    public class CategoryMenuViewComponent : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryMenuViewComponent(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.GetCategories
+                .OrderBy(c => c.Name);
+
+            return View(categories);
+        }
+    }
+}
