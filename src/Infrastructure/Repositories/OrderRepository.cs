2fc8af8451383f977d50b2516a4836959b7c998d
Siarhei Naumau <s.naumau@gmail.com>
2020-08-23 12:07:34 +0300

    feat: implement order interface

---
+++ src/Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,44 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+
+namespace Infrastructure.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly ShoppingCartService _shoppingCartService;
+
+        public OrderRepository(AppDbContext appDbContext, ShoppingCartService shoppingCartService)
+        {
+            _appDbContext = appDbContext;
+            _shoppingCartService = shoppingCartService;
+        }
+
+        public void CreateOrder(Order order)
+        {
+            order.OrderPlaced = DateTime.Now;
+
+            var shoppingCartItems = _shoppingCartService.ShoppingCartItems;
+
+            order.OrderTotal = _shoppingCartService.GetShoppingCartTotal();
+            order.OrderDetails = new List<OrderDetail>();
+
+            foreach (var cartItems in shoppingCartItems)
+            {
+                var orderDetail = new OrderDetail
+                {
+                    Quantity = cartItems.Quantity,
+                    ProductId = cartItems.Product.ProductId,
+                    Price = cartItems.Product.Price,
+                };
+                order.OrderDetails.Add(orderDetail);
+            }
+            _appDbContext.Orders.Add(order);
+            _appDbContext.SaveChanges();
+        }
+    }
+}
