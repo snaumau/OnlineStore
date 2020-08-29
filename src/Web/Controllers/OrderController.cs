32af000b482853bc13ae56f0425b4796e9aeb8b0
Siarhei Naumau <s.naumau@gmail.com>
2020-08-29 08:59:07 +0300

    feat: add order controller

---
+++ src/Web/Controllers/OrderController.cs
@@ -0,0 +1,50 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ShoppingCartService _shoppingCartService;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCartService shoppingCartService)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCartService = shoppingCartService;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var items = _shoppingCartService.GetShoppingCartItems();
+            _shoppingCartService.ShoppingCartItems = items;
+
+            if (_shoppingCartService.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add some products first");
+            }
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCartService.ClearCart();
+
+                return RedirectToAction("CheckoutComplete");
+            }
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thanks for your order!";
+            return View();
+        }
+    }
+}
